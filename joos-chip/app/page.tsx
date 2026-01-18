812611d9839238a29e88541e0a09db485073db98
Ryan Ho <ryanhkho@gmail.com>
2026-01-17 15:54:10 -1000

    redo logic

--- joos-chip/app/page.tsx
+++ joos-chip/app/page.tsx
@@ -8,7 +8,7 @@ import { useState } from "react";
 export default function Home() {
 
   const [passwordAttempt, setPasswordAttempt] = useState("");
-  const password = process.env.NODE_ENV === "production" ? process.env.NEXT_PUBLIC_PASSWORD : process.env.PASSWORD;
+  const password = process.env.NODE_ENV === "production" ? process.env.PASSWORD : process.env.NEXT_PUBLIC_PASSWORD;
 
   return (
     <div className="">
